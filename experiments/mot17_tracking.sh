e47f5ea26812321414b3b070d35bc115d83b6e27
Mohamed Chaabane <chaabanemohamed2@gmail.com>
2021-02-08 15:51:08 -0700

    Update mot17_tracking.sh

--- experiments/mot17_tracking.sh
+++ experiments/mot17_tracking.sh
@@ -1,6 +1,6 @@
 cd src
 # train
-python train.py tracking --exp_id mot17_train --dataset mot --dataset_version 17trainval --pre_hm --ltrb_amodal --same_aug --hm_disturb 0.05 --lost_disturb 0.4 --fp_disturb 0.1 --gpus 0 --load_model ../models/mot17_fulltrain.pth
+python train.py tracking --exp_id mot17_train --dataset mot --dataset_version 17trainval --pre_hm --ltrb_amodal --same_aug --hm_disturb 0.05 --lost_disturb 0.4 --fp_disturb 0.1 --gpus 0 --load_model models/model_mot.pth
 # train the motion model
 python train_prediction.py tracking --exp_id mot17_motion_model --dataset mot --dataset_version 17trainval --gpus 0
 # test
